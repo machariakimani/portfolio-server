229d2cc430c617e465203b5d22b8cb47da74a790
Dev Macharia <devmacharia7@gmail.com>
2026-05-02 01:56:16 +0300

    Added client allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5173",
-  "https://isosoftwares.com",
-  "https://www.isosoftwares.com",
+  "https://devmacharia.vercel.app",
+  "https://www.devmacharia.vercel.app",
 ];
 module.exports = allowedOrigins;
